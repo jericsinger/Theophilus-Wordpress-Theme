555bba194ef7aea736a2c3783e801cf64ed3d75a
Timothy <timothydavidlong@gmail.com>
2013-02-11 13:34:40 -0800

    Initial commit.

---
+++ footer.php
@@ -0,0 +1,19 @@
+<?php
+?>
+</div>
+<div id="footer" class="fix">
+	<div id="footer-container">
+		<p class="left">Copyright &copy; <a href="<?php echo get_settings('home'); ?>/"><?php bloginfo('name'); ?></a></p>
+		<p class="right"><a href="http://www.iamtimothylong.com/theophilus">Theophilus</a> theme by <a href="http://www.iamtimothylong.com">Timothy Long</a><span id="footer-pad">|</span>Powered by <a href="http://wordpress.org">Wordpress</a></p>
+	</div>
+</div>
+<?php wp_footer(); ?>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/functions.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/cufon-yui.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/liberation.font.js"></script>
+<script type="text/javascript">
+	Cufon.replace('h1, h2, h3', {hover: true});
+</script>
+</body>
+</html>
