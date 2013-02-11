37329c914282ea227937cd1494d34fb7f7c63445
Timothy <timothydavidlong@gmail.com>
2013-02-11 14:45:28 -0800

    Latest jQuery build + Updated personal site URL.

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 <div id="footer" class="fix">
 	<div id="footer-container">
 		<p class="left">Copyright &copy; <a href="<?php echo get_settings('home'); ?>/"><?php bloginfo('name'); ?></a></p>
-		<p class="right"><a href="http://www.iamtimothylong.com/theophilus">Theophilus</a> theme by <a href="http://www.iamtimothylong.com">Timothy Long</a><span id="footer-pad">|</span>Powered by <a href="http://wordpress.org">Wordpress</a></p>
+		<p class="right"><a href="http://timothy-long.com/theophilus">Theophilus</a> theme by <a href="http://timothy-long.com">Timothy Long</a><span id="footer-pad">|</span>Powered by <a href="http://wordpress.org">Wordpress</a></p>
 	</div>
 </div>
 <?php wp_footer(); ?>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/functions.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/cufon-yui.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/liberation.font.js"></script>
